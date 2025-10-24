18b9fff0c8837a8cc83de916671ca9e8dba3fa72
Ramy Ramadan <roumio77777@gmail.com>
2025-10-24 04:33:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "exam-73904449-77847",
+  "appId": "1:1095561558703:web:b0ae3143b3259358dd325a",
+  "apiKey": "AIzaSyDNvd5B0U13CtGhbD9rgUq5FrlhMp0M17s",
+  "authDomain": "exam-73904449-77847.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1095561558703"
+};
